ffd8764127615b89634c8457fd8976d81e52e213
UYSALCORP <uysalcorp@gmail.com>
2025-06-10 10:15:31 +0300

    Refactor Customers page and table components for improved structure and loading states

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,34 @@
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-   return <p>Customer Page</p>
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<CustomersTableSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
